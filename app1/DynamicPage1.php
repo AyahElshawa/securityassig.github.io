b8a9dc4a053ea202a4481e5d02d70a50381f42ca
AyahElshawa <81458866+AyahElshawa@users.noreply.github.com>
2022-11-29 16:29:30 +0200

    Update DynamicPage1.php

--- app1/DynamicPage1.php
+++ app1/DynamicPage1.php
@@ -10,17 +10,17 @@ $conn =  mysqli_connect("localhost", "root", "", "security");
 
 // (bug_url) is the URL of the bug file (this file)
 $bugURL= "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
-echo $bugURL."<br>";
+// echo $bugURL."<br>";
 
 //(victim_ip) client ip is the ip of the victim
 $victimIP = $_SERVER['REMOTE_ADDR'];
 // echo $victimIP;
 
 $query = "INSERT INTO app1logger(request_time, victim_ip, bug_url) VALUES ('$requestTime', '$victimIP', '$bugURL')";
-echo $query."<br>";
+// echo $query."<br>";
 
 $result = $conn->query($query);
-echo $result."<br>";
+// echo $result."<br>";
 
 if ($result) {
     echo "New record created successfully";
