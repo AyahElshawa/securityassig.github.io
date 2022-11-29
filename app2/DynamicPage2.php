ee822cd83da310c692bd4cda2042d696394d1ee7
unknown <ayahs19302@gmail.com>
2022-11-29 16:10:58 +0200

    Security Commit

---
+++ app2/DynamicPage2.php
@@ -0,0 +1,30 @@
+<?php
+
+// the time when the page is loaded
+$requestTime = date("Y-m-d H:i:s");
+
+$conn =  mysqli_connect("localhost", "root", "", "security");
+   if (!$conn) {
+       die("Not connected<br>".mysqli_connect_error());
+}
+
+// (bug_url) is the URL of the bug file (this file)
+$bugURL= "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
+echo $bugURL."<br>";
+
+//(victim_ip) client ip is the ip of the victim
+$victimIP = $_SERVER['REMOTE_ADDR'];
+// echo $victimIP;
+
+$query = "INSERT INTO app2logger(request_time, victim_ip, bug_url) VALUES ('$requestTime', '$victimIP', '$bugURL')";
+echo $query;
+
+$result = $conn->query($query);
+
+if ($result) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $query . "<br>" . $conn->error;
+}
+
+?>
